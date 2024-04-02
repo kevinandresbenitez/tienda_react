b73edb8be170200a5b4385c446e1d61831866369
KaBenitez <kevinescudoderoble@gmail.com>
2024-04-02 18:34:00 -0300

    Agregando estilos

--- src/client/app.js
+++ src/client/app.js
@@ -1,20 +1,33 @@
-import React from "react";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Home from "./pages/home/index.js";
-import About from "./pages/about/index.js"
+import React,{ Suspense, lazy }  from "react";
+import {Routes, Route, BrowserRouter, useLocation} from 'react-router-dom';
 import NavBar from "./components/navbar/index.js";
 import { hydrateRoot } from 'react-dom/client';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import Home from './pages/home/index.js';
+import About from './pages/about/index.js';
 
 //Importaciones de css y less
 import './app.less';
+
 export function App(){
+  const location = useLocation();
     return(
       <>
       <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
+      <div className="main">
+      <TransitionGroup>
+          <CSSTransition key={location.key} classNames="fade" timeout={300}>
+            <div className="fullscreen-container"></div>  
+          </CSSTransition>
+
+          <Routes location={location}>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+      </TransitionGroup>
+
+      </div>
       </>
     );
 }
--- src/client/components/navbar/index.js
+++ src/client/components/navbar/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './index.less';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar(){
     return(
-    <div>
-        Esto es la barra de navegacion
-        <Link to="/">Home</Link>
-        <Link to="/about">About us</Link>
+    <div className="navbar">
+        <div className="navbar__logo">
+            <Link to="/">Biba</Link>
+            <Link to="/">Bolsas llenas de color</Link>
+        </div>
+
+        <nav className="navbar__links">
+            <Link to="/">Tienda</Link>
+            <Link to="/about">Acerca De</Link>
+            <Link to="/about">Faq</Link>
+            <Link to="/about">Contacto</Link>
+        </nav>
+        <div className="navbar__actions">
+            <Link to="/about"> <FontAwesomeIcon icon={faUser} />  Iniciar Sesion</Link>
+            <Link to="/about" ><FontAwesomeIcon icon={faCartShopping} /></Link>
+        </div>
+        
+        
     </div>
     )
 }
