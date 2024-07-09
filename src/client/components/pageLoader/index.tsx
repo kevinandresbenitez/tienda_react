4ffc349d1bc2d7154245dccc52f275dc3df5b545
KaBenitez <kevinescudoderoble@gmail.com>
2024-07-09 14:13:52 -0300

    Fix pageloader load

--- src/client/components/pageLoader/index.tsx
+++ src/client/components/pageLoader/index.tsx
@@ -4,10 +4,26 @@ import './index.less';
 export default function PageLoader(){
     const [pageIsLoad,setPageIsLoad] = useState(false);
 
+    const handleLoad = ()=>{
+        setPageIsLoad(true);
+    }
+
     useEffect(()=>{
-        window.addEventListener("load",(event)=>{
+
+        if (document.readyState === "complete") {
             setPageIsLoad(true);
-        })
+
+        } else {
+            document.addEventListener("DOMContentLoaded", handleLoad);
+            window.addEventListener("load", handleLoad);
+        }
+
+
+
+        return () => {
+            document.removeEventListener("DOMContentLoaded", handleLoad);
+            window.removeEventListener("load", handleLoad);
+        }
     },[])
     return(
         pageIsLoad ? (null):(<div className="pageloader-container"><span className="loader"></span></div>)
