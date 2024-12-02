984e38efd19ed02b91ec96bcf48651891ee276f2
KaBenitez <kevinescudoderoble@gmail.com>
2024-12-02 11:19:52 -0300

    Add search bar

--- src/client/pages/home/index.tsx
+++ src/client/pages/home/index.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import {Product} from '../../models/product/index.tsx';
 import {Card} from '../../components/products/index.tsx';
 import './index.less'
+import { SearchBar } from "../../components/common/searchBar/index.tsx";
 export default function Home(){
 
-
+    function onSearch(query:string){}
 
     return(
+        <>
+        <SearchBar  onSearch={onSearch} />
         <article className="articles">
             {Product.getProducts().map((product,key)=>{
             return <Card key={key}>{product}</Card>})}
         </article>
+        </>
 )
 }
