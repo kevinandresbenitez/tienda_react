aa50705c89ffc1bc48bd5f158137e6dcc0a98685
KaBenitez <kevinescudoderoble@gmail.com>
2025-01-01 13:48:30 -0300

    fix img load in server

--- src/client/components/cart/drawerProducts/emptyCart/index.tsx
+++ src/client/components/cart/drawerProducts/emptyCart/index.tsx
@@ -1,15 +1,20 @@
 // @ts-ignore
 import EmpyCartSvg from '../../../../public/imgs/empty_cart.svg';
 import './index.less'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 export function EmptyCart(){
-
+    const [img,setImg] = useState("");
+    
+    useEffect(()=>{
+        setImg(EmpyCartSvg)    
+    },[])
+    
     return(
         <div className='emptyCart'>
             <div className='emptyCart__img'>
-                <img src={"/" + EmpyCartSvg}></img>
+                <img src={img}></img>
             </div>
             <div className='emptyCart__info'>
                 <p>No Hay productos en el carrito</p>
--- src/client/pages/404/index.tsx
+++ src/client/pages/404/index.tsx
@@ -1,13 +1,20 @@
 // @ts-ignore
 import NotFoundIMG from '../../public/imgs/404_notFound.svg';
 import './index.less'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function PageNotFound(){
+
+    const [img,setImg] = useState("");
+    
+    useEffect(()=>{
+        setImg(NotFoundIMG)    
+    },[])
+
     return(
         <div className='container'>            
             <div className="container__img">
-                <img src={NotFoundIMG}></img>
+                <img src={img}></img>
             </div>
 
             <div className="container__info">
